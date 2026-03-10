03aed807265a00a3f5152d89d9f6dfc116a0715a
Devansh Sharma <dksharma21810@gmail.com>
2026-03-10 21:06:53 +0530

    Update student names in names_pool.py

--- backend/names_pool.py
+++ backend/names_pool.py
@@ -1,9 +1,9 @@
 STUDENTS = [
     "Ayyan", "Adarsh", "Alisha", "Angel", "Ananya", "Arjun", "Arnav", "Avni",
     "Ayaan", "Bhavya", "Chirag", "Devansh", "Dhruv", "Diya", "Eshan", "Farhan",
-    "Gaurav", "Harsh", "Ishaan", "Jiya", "Kabir", "Kavya", "Krish", "Lakshmi",
-    "Manav", "Meera", "Mihir", "Naman", "Neha", "Nikhil", "Nisha", "Om",
-    "Parth", "Pooja", "Pratyaksh", "Pranjal", "Rahul", "Ranya", "Rishi", "Rohit",
+    "Gaurav", "Harsh Verma", "Ishaan", "Jiya Agrawal", "Jiya Mukhija", "Kavya", "Krish", "Lakshmi",
+    "Manav", "Meera", "Mihir", "Naman", "Nancy", "Nikhil", "Nisha", "Om",
+    "Parth", "Pooja", "Pratyaksh", "Pranjal", "Rahul", "Rakshita", "Rishi", "Rohit",
     "Saanvi", "Sahil", "Saksham", "Shreya", "Siddharth", "Sneha", "Tanvi", "Yash",
 ]
 
